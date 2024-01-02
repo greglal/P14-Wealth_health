a816fb86019c74b802b008f607eda553165bd1cc
LAPTOP-LCRCBR4G\glall <g.lallemand@gmail.com>
2024-01-02 12:14:49 +0100

    1st commit

---
+++ wealth-health/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './Styles/index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+
+    </Router>
+  </React.StrictMode>
+);
+
+
